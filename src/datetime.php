3c91b4c350f6e24085cf4405f2d4a1b4111bd994
Saulius Vaičeliūnas <saulius.vaiceliunas@telesoftas.com>
2020-12-03 23:09:38 +0200

    fix: php8 deprecation warning for required parameters order

--- src/datetime.php
+++ src/datetime.php
@@ -30,7 +30,7 @@ function elapsed_time($date, array $labels = [], $format = ELAPSED_TIME_FORMAT_F
 /**
  * @throws \Exception
  */
-function countdown($dateFrom = 'now', $dateTo, string $format = '%s%02d:%02d:%02d'): string
+function countdown($dateFrom = 'now', $dateTo = null, string $format = '%s%02d:%02d:%02d'): string
 {
     return OperationFactory::create(DateTimeOperation\Countdown::class)->execute($dateFrom, $dateTo, $format);
 }
